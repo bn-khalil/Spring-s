64e3b8d9faf6b87087a42845d6d0127d6aaf6393
bn-khalil <benkhaliltouda3201@gmail.com>
2025-08-15 11:19:12 +0100

    using Request param to get parameter from request

--- firstboot/src/main/java/com/example/firstboot/controller/FirstController.java
+++ firstboot/src/main/java/com/example/firstboot/controller/FirstController.java
@@ -1,12 +1,19 @@
 package com.example.firstboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
-    @RequestMapping("/")
-    public String home() {
-        return ("home.html");
+    @GetMapping("/home/{name}")
+    public String home(@RequestParam String name,
+                       @RequestParam String lastname,
+                       Model page ) {
+        page.addAttribute("name", name);
+//        page.addAttribute("lastname", lastname);/
+        return ("render.html");
     }
 }
