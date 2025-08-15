7dc5b2b577e4103686d9386049320391ac8583d6
bn-khalil <benkhaliltouda3201@gmail.com>
2025-08-15 11:22:15 +0100

    get path variable form request

--- firstboot/src/main/java/com/example/firstboot/controller/FirstController.java
+++ firstboot/src/main/java/com/example/firstboot/controller/FirstController.java
@@ -3,15 +3,14 @@ package com.example.firstboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
     @GetMapping("/home/{name}")
-    public String home(@RequestParam String name,
-                       @RequestParam String lastname,
-                       Model page ) {
+    public String home(@PathVariable String name, Model page ) {
         page.addAttribute("name", name);
 //        page.addAttribute("lastname", lastname);/
         return ("render.html");
